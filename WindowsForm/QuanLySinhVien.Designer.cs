4dfe902ec4d11566259e27ef6cc8079a730f25a1
QuangBach <bachh1124@gmail.com>
2024-03-30 16:39:17 +0700

    Add project files.

---
+++ WindowsForm/QuanLySinhVien.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WindowsForm
+{
+    partial class QuanLySinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // QuanLySinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "QuanLySinhVien";
+            this.Text = "Quản lý sinh viên";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
