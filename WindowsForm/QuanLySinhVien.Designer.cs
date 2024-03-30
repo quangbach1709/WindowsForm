65c4f2e6b03c961999efa1cb4dcb487591593b76
QuangBach <bachh1124@gmail.com>
2024-03-30 16:40:23 +0700

    upload

--- WindowsForm/QuanLySinhVien.Designer.cs
+++ WindowsForm/QuanLySinhVien.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "QuanLySinhVien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý sinh viên";
             this.ResumeLayout(false);
 
