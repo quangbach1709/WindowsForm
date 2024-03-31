9d3f69422bc8cf7a1b3981fc945e1b70c14e4503
QuangBach <bachh1124@gmail.com>
2024-03-31 22:59:30 +0700

    QL truyen tranh

--- WindowsForm/Program.cs
+++ WindowsForm/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new QuanLyBanHang());
+            Application.Run(new QuanLyTruyenTranh());
         }
     }
 }
